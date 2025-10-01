da056bddf3f3333da24d93ebed45899d913bf47c
juno <juno45381@gmail.com>
2025-10-01 17:44:28 +0800

    added deposit and withdraw features but not done yet

--- script.js
+++ script.js
@@ -1,13 +1,27 @@
-const createAccountBtn = document.querySelector(".create-account-btn");
-const createAccount = document.querySelector(".create-account")
+const depositBtn = document.querySelector(".deposit-btn");
+const withdrawBtn = document.querySelector(".withdraw-btn");
 let balance = 0;
-createAccountBtn.addEventListener("click", () => {
-   let createAccountInput = document.querySelector(".create-account-input");
-   createAccountInput.style.display = "block";
-   let createUser = createAccountInput.value;
-   createAccountInput.value = ""
-   createAccountInput.focus();
-   console.log(createUser);
-})
+depositBtn.addEventListener("click", () => {
+    let depositInput = document.querySelector(".deposit-input");
+    let depositLabel = document.querySelector(".label-deposit");
+    depositInput.style.display = "block";
+    depositLabel.style.display = "block";
+    balance = Number(depositInput.value);
+    console.log(balance);
+});
+withdrawBtn.addEventListener("click", () => {
+    let withdrawInput = document.querySelector(".withdraw-input");
+    let withdrawLabel = document.querySelector(".label-withdraw");
+    withdrawLabel.style.display = "block";
+    withdrawInput.style.display = "block";
+    let withdrawValue = withdrawInput.value;
+    if (withdrawValue > balance) {
+        console.log("tite");
+    }else {
+        let currentBalance = balance - withdrawValue;
+        balance = currentBalance;
+        console.log(`Withdraw successfully. Your current balance is ${balance}`);
+    }
 
 
+});
