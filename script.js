28ff9b3106f8f63abac2fe62571631ccf0d534c3
juno <juno45381@gmail.com>
2025-10-03 17:36:36 +0800

    lapit na

--- script.js
+++ script.js
@@ -1,47 +1,65 @@
 const depositBtn = document.querySelector(".deposit-btn");
 const withdrawBtn = document.querySelector(".withdraw-btn");
-const checkBalance = document.querySelector(".check-balance-btn");
+const checkBalanceBtn = document.querySelector(".check-balance-btn");
+const confirmDepositBtn = document.querySelector(".confirm-deposit-btn");
+const confirmWithdrawBtn = document.querySelector(".confirm-withdraw-btn");
 let currentBalanceText = document.createElement("div");
 let insufficientFundsText = document.createElement("div");
+let deposit = document.querySelector(".deposit")
+let depositInput = document.querySelector(".deposit-input");
+let depositLabel = document.querySelector(".label-deposit");
+let withdraw = document.querySelector(".withdraw")
+let withdrawInput = document.querySelector(".withdraw-input");
+let withdrawLabel = document.querySelector(".label-withdraw");
+let checkBalanceDiv = document.createElement("div");
+let checkBalance = document.querySelector(".check-balance")
 let balance = 0;
 let password = "";
 let correctPassword = "12345";
-while (password !== correctPassword) {
+/*while (password !== correctPassword) {
     password = prompt("Enter password");
-    
-    depositBtn.addEventListener("click", () => {
-        let deposit = document.querySelector(".deposit")
-        let depositInput = document.querySelector(".deposit-input");
-        let depositLabel = document.querySelector(".label-deposit");
-        depositInput.style.display = "block";
-        depositLabel.style.display = "block";
-        balance = Number(depositInput.value);
-        currentBalanceText.textContent = (`Your current balance is ${balance}`);
-        deposit.append(currentBalanceText);
-        console.log(balance);
-    });
-    withdrawBtn.addEventListener("click", () => {
-        let withdraw = document.querySelector(".withdraw")
-        let withdrawInput = document.querySelector(".withdraw-input");
-        let withdrawLabel = document.querySelector(".label-withdraw");
-        withdrawLabel.style.display = "block";
-        withdrawInput.style.display = "block";
-        let withdrawValue = Number(withdrawInput.value);
-        
-        if (withdrawValue > balance) {
-            insufficientFundsText.textContent = "Insufficient Funds";
-            withdraw.append(insufficientFundsText);
-        }else {
-            let currentBalance = balance - withdrawValue;
-            balance = currentBalance;
-            currentBalanceText.textContent = balance;
+}*/
+depositBtn.addEventListener("click", () => {
+    depositInput.style.display = "block";
+    depositLabel.style.display = "block";
+    confirmDepositBtn.style.display = "block"
+    depositBtn.disabled = true;
+});
+confirmDepositBtn.addEventListener("click", () => {
+    balance += Number(depositInput.value);
+        if (balance !== 0) {
             currentBalanceText.textContent = (`Your current balance is ${balance}`);
-            withdraw.appendChild(currentBalanceText);
+            deposit.append(currentBalanceText);
+            confirmDepositBtn.style.display = "none";
+            depositBtn.disabled = false;
         }
-    });
-    checkBalance.addEventListener("click", () => {
-        let checkBalance = document.querySelector(".check-balance");
+    depositInput.focus();
+    depositInput.value = "";
+});
+withdrawBtn.addEventListener("click", () => {
+    withdrawLabel.style.display = "block";
+    withdrawInput.style.display = "block";
+    confirmWithdrawBtn.style.display = "block";
+    withdrawBtn.disabled = true;
+});
+confirmWithdrawBtn.addEventListener("click", () => {
+    let withdrawValue = Number(withdrawInput.value);
+    if (withdrawValue > balance) {
+        insufficientFundsText.textContent = "Insufficient Funds";
+        withdraw.append(insufficientFundsText);
+    }else {
+        let currentBalance = balance - withdrawValue;
+        balance = currentBalance;
+        currentBalanceText.textContent = balance;
         currentBalanceText.textContent = (`Your current balance is ${balance}`);
-        checkBalance.append(currentBalanceText);
-    })
-}
+        insufficientFundsText.textContent = "";
+        withdrawBtn.disabled = false;
+    }
+    withdrawInput.value = "";
+    withdrawInput.focus();
+
+});
+checkBalanceBtn.addEventListener("click", () => {
+    checkBalanceDiv.textContent = (`Your updated balance is ${balance}`);
+    checkBalance.append(checkBalanceDiv)
+})
