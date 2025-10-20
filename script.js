873deba0dbdaa1b1f08f354ff9c7c48056d4c3f2
juno <juno45381@gmail.com>
2025-10-20 15:29:32 +0800

    put a recent transaction feature

--- script.js
+++ script.js
@@ -1,22 +1,23 @@
 const depositBtn = document.querySelector(".deposit-btn");
 const withdrawBtn = document.querySelector(".withdraw-btn");
-const checkBalanceBtn = document.querySelector(".check-balance-btn");
 const confirmDepositBtn = document.querySelector(".confirm-deposit-btn");
 const confirmWithdrawBtn = document.querySelector(".confirm-withdraw-btn");
 let currentBalanceText = document.createElement("div");
 let insufficientFundsText = document.createElement("div");
-let deposit = document.querySelector(".deposit")
+let deposit = document.querySelector(".deposit");
 let depositInput = document.querySelector(".deposit-input");
 let depositLabel = document.querySelector(".label-deposit");
-let withdraw = document.querySelector(".withdraw")
+let withdraw = document.querySelector(".withdraw");
 let withdrawInput = document.querySelector(".withdraw-input");
 let withdrawLabel = document.querySelector(".label-withdraw");
-let mainBalance = document.querySelector(".main-balance")
-let showBalance = document.querySelector(".show-balance")
+let mainBalance = document.querySelector(".main-balance");
+let showBalance = document.querySelector(".show-balance");
+let recentTransaction = document.querySelector(".recent-transaction");
+let transactionList = document.querySelector(".transaction-list");
 let balance = 0;
-let password = "";
+/*let password = "";
 let correctPassword = "12345";
-/*while (password !== correctPassword) {
+while (password !== correctPassword) {
     password = prompt("Enter password");
 }*/
 depositBtn.addEventListener("click", () => {
@@ -28,11 +29,15 @@ depositBtn.addEventListener("click", () => {
 confirmDepositBtn.addEventListener("click", () => {
     balance += Number(depositInput.value);
         if (balance !== 0) {
-            currentBalanceText.textContent = (`Your current balance is ${balance}`);
+            currentBalanceText.textContent = (`Your current balance is $${balance}`);
             deposit.append(currentBalanceText);
             confirmDepositBtn.style.display = "none";
             depositBtn.disabled = false;
         }
+
+        const li = document.createElement("li");
+        li.textContent = `Deposit: $${depositInput.value}`;
+        transactionList.append(li)
     depositInput.focus();
     depositInput.value = "";
     showBalance.textContent = balance;
@@ -53,16 +58,19 @@ confirmWithdrawBtn.addEventListener("click", () => {
         let currentBalance = balance - withdrawValue;
         balance = currentBalance;
         currentBalanceText.textContent = balance;
-        currentBalanceText.textContent = (`Your current balance is ${balance}`);
+        currentBalanceText.textContent = (`Your current balance is $${balance}`);
         insufficientFundsText.textContent = "";
         withdrawBtn.disabled = false;
     }
+    const li = document.createElement("li");
+    if (insufficientFundsText.textContent === "Insufficient Funds") {
+        return "";
+    }else {
+        li.textContent = `Withdrew:$ ${withdrawValue}`;
+        transactionList.append(li)
+    }
     withdrawInput.value = "";
     withdrawInput.focus();
-    showBalance.textContent = balance.toLocaleString("en-PH", {
-        style: "currency",
-        currency: "PHP"
-    });
+    showBalance.textContent = balance;
     mainBalance.append(showBalance);
 });
-
